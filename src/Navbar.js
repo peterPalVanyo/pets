3002cc8af7bb2f68d5bef9c7d258989f3b0327b9
peterPalVanyo <vanyo.peter.pal@gmail.com>
2019-11-17 17:08:12 +0100

    imports bootstrap, router, jquery, proper and images

---
+++ src/Navbar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react'
+
+class Navbar extends Component {
+
+    render() {
+        return (
+            <h1>From Navbar</h1>
+        )
+    }
+}
+
+export default Navbar
